1b89eb48d13242d80e89719e6ed84f05d1864cb3
frank2 <frank2@dc949.org>
2024-11-18 01:33:00 -0800

    add the shellcode tester

---
+++ test.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <windows.h>
+
+extern uint8_t SHELLCODE_DATA[];
+
+int main(int argc, char *argv[]) {
+   uint8_t *valloc_buffer = VirtualAlloc(NULL, SHELLCODE_SIZE, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+
+   if (valloc_buffer == NULL)
+      return 1;
+
+   memcpy(valloc_buffer, &SHELLCODE_DATA[0], SHELLCODE_SIZE);
+
+   return ((int (*)(LPVOID))valloc_buffer)(GetModuleHandle(NULL)) != 0;
+}
+
+   
